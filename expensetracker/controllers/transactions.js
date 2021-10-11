d5e1b9e0c5c819439421b7c647edb1e252671875
kaviya bakiyavel <kaviya.bakiyavel@iqvia.com>
2021-10-11 23:06:08 +0530

    expense tracker

---
+++ expensetracker/controllers/transactions.js
@@ -0,0 +1,25 @@
+//methods and getting route set up
+
+
+// @desc get all transactions
+// @route GET /api/v1/transactions
+// @access public
+exports.getTransactions = (req, res, next) => {
+    res.send('GET transactions')
+}
+
+
+// @desc Add transactions
+// @route POST /api/v1/transactions
+// @access public
+exports.addTransaction = (req, res, next) => {
+    res.send('POST transactions')
+}
+
+
+// @desc Delete transactions
+// @route DELETE /api/v1/transactions
+// @access public
+exports.deleteTransaction = (req, res, next) => {
+    res.send('DELETE transactions')
+}
---
+++ expensetracker/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const transactions = require('./routes/transactions');
+const connectDB = require('./config/db');
+
+dotenv.config({path : './config/config.env'});
+
+connectDB();
+
+const app = express();
+app.use('/api/v1/transactions', transactions);
+app.get('/',(req,res) => res.send('Hello'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
